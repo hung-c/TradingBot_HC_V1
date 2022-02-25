c40bf70a5e7e3d0a51d9bae2b8e6b5f3b197ee28
hung-c <hcao92554@gmail.com>
2022-02-25 12:46:29 -0600

    Add index.js and connect functions

---
+++ index.js
@@ -0,0 +1,41 @@
+const ccxt = require('ccxt');
+const moment = require('moment');
+
+//Connect to binance
+const binance = new ccxt.binance({
+    apiKey: '',
+    secret: ''
+    // need your apikey and secret key from your binance account to connect to market
+});
+binance = ccxt.binance({ 'options': { 'adjustForTimeDifference': True }})
+binance.setSandboxMode(true); //connect to demo test web
+
+//Print your balance
+async function printBalance() {
+    const balance = await binance.fetchBalance(); 
+    console.log(balance);
+}
+
+/*async function main() {
+    
+    //Get Price of BTC
+    const price = await binance.fetchOHLCV ('BTC/USDT', '1m',undefined, 5)
+    const bPrices = price.map(price => {
+        return {
+            timestamp: moment(price[0]).format(),
+            open: price[1],
+            high: price[2],
+            low: price[3],
+            close: price[4],
+            volume: price[5]
+        }
+    } )
+    console.log(bPrices);
+    const average5Prices = bPrices.reduce((acc,price) => acc + price.close, 0)/5 
+    const lastPrice =bPrices[bPrices.length-1].close;
+
+
+    console.log(bPrices.map(p => p.close),average5Prices, lastPrice);
+}*/
+
+printBalance()
