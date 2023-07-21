1660e7ff2104dd5bfe9c4d4e8a18ba50ca2d9b48
hcao <HCao@bend-tech.com>
2023-07-21 14:11:31 -0500

    [Addition] Add express server

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const BinanceAPI = require('./index.js');
+
+const app = express(); 
+
+// Setup public folder 
+app.use(express.static(path.join(__dirname,'public')));
+
+// Body parser middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+//API configs 
+const port = 3000;
+
+// Replace these with your actual Binance API key and secret
+const apiKey = process.env.KEY;
+const apiSecret = process.env.SECRET;
+
+const binanceAPI = new BinanceAPI(apiKey, apiSecret);
+
+//Start server
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
+// Route to get the latest prices in JSON format
+app.get('/prices', async (req, res) => {
+    try {
+      const currency = req.query.currency || 'BTC/USDT';
+      const numberOfPrices = parseInt(req.query.numberOfPrices) || 1;
+  
+      const prices = await binanceAPI.getPrices(currency, numberOfPrices);
+      res.json(prices);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred while fetching the prices.' });
+    }
+});
--- index.js
+++ index.js
@@ -1,73 +1,94 @@
 const ccxt = require('ccxt');
 const moment = require('moment');
 const delay = require('delay');
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require('dotenv').config();
 
-//Connect to binance
-const binance = new ccxt.binance({
-    apiKey: process.env.KEY,
-    secret: process.env.SEC
-    // need your apikey and secret key from your binance account to connect to market
-});
-binance.setSandboxMode(true); //connect to demo test web
+class BinanceAPI {
+    constructor(key, secret)
+    {
+        this.binance = new ccxt.binance({
+            // Need your apikey and secret key from your binance account to connect to market
+            apiKey: key,
+            secret: secret
+            
+        });
+        this.binance.setSandboxMode(true); //connect to demo test web
+    }
 
-//Print your balance
-async function printBalance(btcPrice) {
-    const balance = await binance.fetchBalance(); 
-    const total = balance.total
-    console.log(`Balance: BTC ${total.BTC}, USDT: ${total.USDT}`);
-    console.log(`Total USDT: ${(total.BTC-1) * btcPrice + total.USDT}`);
+    async printBalance(btcPrice){
+        const balance = await this.binance.fetchBalance(); 
+        const total = balance.total
+        console.log(`Balance: BTC ${total.BTC}, USDT: ${total.USDT}`);
+        console.log(`Total USDT: ${(total.BTC-1) * btcPrice + total.USDT}`);
+    }
+    
+    async getPrices(currency= 'BTC/USDT', numberOfPrices = 1) {
+        const price = await this.binance.fetchOHLCV (currency, '1m',undefined, numberOfPrices)
+        const bPrices = price.map(price => {
+            return {
+                timestamp: moment(price[0]).toLocaleString(),
+                open: price[1],
+                high: price[2],
+                low: price[3],
+                close: price[4],
+                volume: price[5]
+            }
+        });
+        return price;
+    }
 
+    async tick() {
+
+        //Get Price of BTC
+        console.log(' ');
+        const price = await this.binance.fetchOHLCV ('BTC/USDT', '1m',undefined, 5)
+        const bPrices = price.map(price => {
+            return {
+                timestamp: moment(price[0]).toLocaleString(),
+                open: price[1],
+                high: price[2],
+                low: price[3],
+                close: price[4],
+                volume: price[5]
+            }
+        } )
+   
+        console.log(bPrices);
+        const averagePrices = bPrices.reduce((acc,price) => acc + price.close, 0)/5 
+        const lastPrice =bPrices[bPrices.length-1].close
+        console.log(bPrices.map(p => p.close),averagePrices, lastPrice);
+        console.log(`Average price: ${averagePrices}. Last Price: ${lastPrice}`);
+   
+        //Algorithm to make decision 
+        let direction = ''
+        if (lastPrice > averagePrices)
+        {
+            direction = 'sell';
+        }
+        else {
+            direction = 'buy';
+        }
+    
+        const TRADE_SIZE = 100;
+        const quantity = TRADE_SIZE / lastPrice
+        
+        //const order = await binance.createOrder('BTC/USDT', direction, 'limit', quantity)
+        const order = await this.binance.createMarketOrder('BTC/USDT', direction, quantity);
+        console.log(`${moment().toLocaleString()}: ${direction} ${quantity} BTC at ${lastPrice}`)
+        //console.log(order);
+        this.printBalance(lastPrice);
+   }
 }
 
-async function tick() {
-
-     //Get Price of BTC
-     const price = await binance.fetchOHLCV ('BTC/USDT', '1m',undefined, 5)
-     const bPrices = price.map(price => {
-         return {
-             timestamp: moment(price[0]).toLocaleString(),
-             open: price[1],
-             high: price[2],
-             low: price[3],
-             close: price[4],
-             volume: price[5]
-         }
-     } )
-
-     console.log(bPrices);
-     const averagePrices = bPrices.reduce((acc,price) => acc + price.close, 0)/5 
-     const lastPrice =bPrices[bPrices.length-1].close
-     console.log(bPrices.map(p => p.close),averagePrices, lastPrice);
-     console.log(`Average price: ${averagePrices}. Last Price: ${lastPrice}`);
-
-     //Algorithm to make decision 
-     let direction = ''
-     if (lastPrice > averagePrices)
-     {
-         direction = 'sell';
-     }
-     else {
-         direction = 'buy';
-     }
- 
-     const TRADE_SIZE = 100;
-     const quantity = TRADE_SIZE / lastPrice
-     
-     //const order = await binance.createOrder('BTC/USDT', direction, 'limit', quantity)
-     const order = await binance.createMarketOrder('BTC/USDT', direction, quantity);
-     console.log(`${moment().toLocaleString()}: ${direction} ${quantity} BTC at ${lastPrice}`)
-     //console.log(order);
-     printBalance(lastPrice);
-}
-
+module.exports = BinanceAPI
 async function main() {
     while(true) {
-        await tick();
+        Binace1 = new BinanceAPI(process.env.KEY, process.env.SECRET)
+        prices =  await Binace1.getPrices('BTC/USDT', 10);
+        console.log(prices);
         await delay(60 * 1000);
     }
 }
 
 //printBalance()
-main()
+//main()
